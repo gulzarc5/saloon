554b226e24bf2b7d2522e827e2f4618abd710d13
gulzarc5 <gulzarc5@gmail.com>
2020-12-25 12:53:22 +0530

    u

--- app/Http/Controllers/Api/AppSettingController.php
+++ app/Http/Controllers/Api/AppSettingController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Client;
 use App\Models\JobCategory;
 use Illuminate\Http\Request;
 use App\Models\ServiceCity;
 use App\Models\Slider;
+use DB;
 
 class AppSettingController extends Controller
 {
@@ -25,14 +27,22 @@ class AppSettingController extends Controller
     {
         $Sliders = Slider::get();
         $category_list = JobCategory::where('status',1)->get();
+        $top_saloon = Client::where('profile_status',2)->where('job_status',2)->where('status',1)->where('clientType',1)->withCount(['review as average_rating' => function($query) {
+            $query->select(DB::raw('coalesce(avg(rating),0)'));
+        }])->orderByDesc('average_rating')->limit(9)->get();
+        $top_free_launcer = Client::where('profile_status',2)->where('job_status',2)->where('status',1)->where('clientType',2)->withCount(['review as average_rating' => function($query) {
+            $query->select(DB::raw('coalesce(avg(rating),0)'));
+        }])->orderByDesc('average_rating')->limit(10)->get();
+
+        dd($top_saloon);
         $response = [
             'status' => true,
             'message' => 'Service City List',
             'data' => [
                 'sliders' => $Sliders,
                 'category' => $category_list,
-                'top_saloon' => [],
-                'top_free_launcer' => [],
+                'top_saloon' => $top_saloon,
+                'top_free_launcer' => $top_free_launcer,
             ],
         ];
         return response()->json($response, 200);
